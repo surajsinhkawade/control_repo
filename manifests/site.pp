4b6df37cf0fe8a1767c84afb47c796999ee8804a
surajsinhkawade <33827652+surajsinhkawade@users.noreply.github.com>
2018-09-29 22:13:41 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,6 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is readme file',
-    owner   => 'root',
-  }
-  
+}
+
+node 'master.puppet.vm' {
+  include role::master_server
 }
