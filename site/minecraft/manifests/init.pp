4152f85432a458fdc6f4d6ef675e8134b9422385
surajsinhkawade <33827652+surajsinhkawade@users.noreply.github.com>
2018-09-30 22:50:37 +0530

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,18 +1,22 @@
 class minecraft {
-  file {'/opt/minecraft':
+    $url = 'https://launcher.mojang.com/v1/objects/fe123682e9cb30031eae351764f653500b7396c9/server.jar'
+    $install_dir = '/opt/minecraft'
+  
+  file {$install_dir:
     ensure => directory,
    }
    
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
+    source => $url,
+    before => Service['minecraft'],
   }
   
   package {'java':
     ensure => present,
   }
   
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
@@ -25,5 +29,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
