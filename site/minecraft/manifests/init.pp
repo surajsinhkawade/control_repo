cd6a0f8e00ae2335d4496337a6f0de092a0ea291
surajsinhkawade <33827652+surajsinhkawade@users.noreply.github.com>
2018-09-30 21:35:04 +0530

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -21,4 +21,9 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  
+  service {'minecraft':
+    ensure => running,
+    enable => true,
+  }
 }
